b0bc387d76a6cf36d46180747cc782d0231dca70
sungeon <ryuee091119@naver.com>
2024-06-03 16:45:26 +0900

    구구단 업그레이드

--- 240603_6차시_반복문.c
+++ 240603_6차시_반복문.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 
+#define SECOND_START 1
+#define SECOND_LIMIT 9
 #define FIRST_START 2
-#define FIRST_LIMIT 99
-#define SECOND_LIMIT 1
-#define SECOND_LIMIT 99
+#define FIRST_LIMIT 9
+
+#define INPUT
+
+void googoodan(int first, int secondStart, int SecondEnd)
+{
+    printf("%d단\n", first);
+    for (int secondNum = secondStart; secondNum <= SecondEnd; secondNum++)
+    {
+        printf("%d X %d = %d\n", first, secondNum, secondNum * first);
+    }
+}
 
 int main()
 {
+#ifdef INPUT
+    int firstNum;
+    scanf("%d", &firstNum);
+    googoodan(firstNum, SECOND_START, SECOND_LIMIT);
+#else
     for (int firstNum = 2; firstNum <= FIRST_LIMIT; firstNum++)
     {
-        printf("%d단\n", firstNum);
-        for (int secondNum = 1; secondNum <= SECOND_LIMIT; secondNum++)
-        {
-            printf("%d X %d = %d\n", firstNum, secondNum, secondNum * firstNum);
-        }
-        printf("\n");
+        googoodan(firstNum, SECOND_START, SECOND_LIMIT)
+            printf("\n");
     }
+#endif
     return 0;
 }
